16701b10c9462fdeeb6a4dd7613db3ef1e2c12cc
Stephen Jensen <stephen.jensen3@gmail.com>
2019-07-01 18:28:51 -0500

    added rest of queries

--- frontend/generated/apolloComponents.tsx
+++ frontend/generated/apolloComponents.tsx
@@ -1,6 +1,6 @@
 import gql from "graphql-tag";
-import * as React from "react";
 import * as ReactApollo from "react-apollo";
+import * as React from "react";
 export type Maybe<T> = T | null;
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 /** All built-in and custom scalars, mapped to their actual values */
@@ -101,26 +101,179 @@ export type UserCreateInput = {
   name?: Maybe<Scalars["String"]>;
   posts?: Maybe<PostCreateManyWithoutPostsInput>;
 };
+export type PostFragmentFragment = { __typename?: "Post" } & Pick<
+  Post,
+  "id" | "published" | "title" | "content" | "published"
+>;
+
+export type UserFragmentFragment = { __typename?: "User" } & Pick<
+  User,
+  "id" | "name" | "email"
+>;
+
+export type CreateDraftMutationMutationVariables = {
+  title: Scalars["String"];
+  content: Scalars["String"];
+  authorEmail: Scalars["String"];
+};
+
+export type CreateDraftMutationMutation = { __typename?: "Mutation" } & {
+  createDraft: { __typename?: "Post" } & PostFragmentFragment;
+};
+
+export type PublishMutationMutationVariables = {
+  id: Scalars["ID"];
+};
+
+export type PublishMutationMutation = { __typename?: "Mutation" } & {
+  publish: Maybe<{ __typename?: "Post" } & PostFragmentFragment>;
+};
+
+export type SignupUserMutationMutationVariables = {
+  name: Scalars["String"];
+  email: Scalars["String"];
+};
+
+export type SignupUserMutationMutation = { __typename?: "Mutation" } & {
+  signupUser: { __typename?: "User" } & UserFragmentFragment;
+};
+
 export type FeedQueryQueryVariables = {};
 
 export type FeedQueryQuery = { __typename?: "Query" } & {
-  feed: Array<
-    { __typename?: "Post" } & Pick<
-      Post,
-      "id" | "published" | "title" | "content"
-    >
-  >;
+  feed: Array<{ __typename?: "Post" } & PostFragmentFragment>;
 };
+
+export type PostQueryQueryVariables = {
+  id: Scalars["ID"];
+};
+
+export type PostQueryQuery = { __typename?: "Query" } & {
+  post: Maybe<{ __typename?: "Post" } & PostFragmentFragment>;
+};
+export const PostFragmentFragmentDoc = gql`
+  fragment PostFragment on Post {
+    id
+    published
+    title
+    content
+    published
+  }
+`;
+export const UserFragmentFragmentDoc = gql`
+  fragment UserFragment on User {
+    id
+    name
+    email
+  }
+`;
+export const CreateDraftMutationDocument = gql`
+  mutation createDraftMutation(
+    $title: String!
+    $content: String!
+    $authorEmail: String!
+  ) {
+    createDraft(title: $title, content: $content, authorEmail: $authorEmail) {
+      ...PostFragment
+    }
+  }
+  ${PostFragmentFragmentDoc}
+`;
+export type CreateDraftMutationMutationFn = ReactApollo.MutationFn<
+  CreateDraftMutationMutation,
+  CreateDraftMutationMutationVariables
+>;
+export type CreateDraftMutationComponentProps = Omit<
+  ReactApollo.MutationProps<
+    CreateDraftMutationMutation,
+    CreateDraftMutationMutationVariables
+  >,
+  "mutation"
+>;
+
+export const CreateDraftMutationComponent = (
+  props: CreateDraftMutationComponentProps
+) => (
+  <ReactApollo.Mutation<
+    CreateDraftMutationMutation,
+    CreateDraftMutationMutationVariables
+  >
+    mutation={CreateDraftMutationDocument}
+    {...props}
+  />
+);
+
+export const PublishMutationDocument = gql`
+  mutation publishMutation($id: ID!) {
+    publish(id: $id) {
+      ...PostFragment
+    }
+  }
+  ${PostFragmentFragmentDoc}
+`;
+export type PublishMutationMutationFn = ReactApollo.MutationFn<
+  PublishMutationMutation,
+  PublishMutationMutationVariables
+>;
+export type PublishMutationComponentProps = Omit<
+  ReactApollo.MutationProps<
+    PublishMutationMutation,
+    PublishMutationMutationVariables
+  >,
+  "mutation"
+>;
+
+export const PublishMutationComponent = (
+  props: PublishMutationComponentProps
+) => (
+  <ReactApollo.Mutation<
+    PublishMutationMutation,
+    PublishMutationMutationVariables
+  >
+    mutation={PublishMutationDocument}
+    {...props}
+  />
+);
+
+export const SignupUserMutationDocument = gql`
+  mutation signupUserMutation($name: String!, $email: String!) {
+    signupUser(data: { name: $name, email: $email }) {
+      ...UserFragment
+    }
+  }
+  ${UserFragmentFragmentDoc}
+`;
+export type SignupUserMutationMutationFn = ReactApollo.MutationFn<
+  SignupUserMutationMutation,
+  SignupUserMutationMutationVariables
+>;
+export type SignupUserMutationComponentProps = Omit<
+  ReactApollo.MutationProps<
+    SignupUserMutationMutation,
+    SignupUserMutationMutationVariables
+  >,
+  "mutation"
+>;
+
+export const SignupUserMutationComponent = (
+  props: SignupUserMutationComponentProps
+) => (
+  <ReactApollo.Mutation<
+    SignupUserMutationMutation,
+    SignupUserMutationMutationVariables
+  >
+    mutation={SignupUserMutationDocument}
+    {...props}
+  />
+);
 
 export const FeedQueryDocument = gql`
   query feedQuery {
     feed {
-      id
-      published
-      title
-      content
+      ...PostFragment
     }
   }
+  ${PostFragmentFragmentDoc}
 `;
 export type FeedQueryComponentProps = Omit<
   ReactApollo.QueryProps<FeedQueryQuery, FeedQueryQueryVariables>,
@@ -134,25 +287,23 @@ export const FeedQueryComponent = (props: FeedQueryComponentProps) => (
   />
 );
 
-export type FeedQueryProps<TChildProps = {}> = Partial<
-  ReactApollo.DataProps<FeedQueryQuery, FeedQueryQueryVariables>
+export const PostQueryDocument = gql`
+  query postQuery($id: ID!) {
+    post(where: { id: $id }) {
+      ...PostFragment
+    }
+  }
+  ${PostFragmentFragmentDoc}
+`;
+export type PostQueryComponentProps = Omit<
+  ReactApollo.QueryProps<PostQueryQuery, PostQueryQueryVariables>,
+  "query"
 > &
-  TChildProps;
-export function withFeedQuery<TProps, TChildProps = {}>(
-  operationOptions?: ReactApollo.OperationOption<
-    TProps,
-    FeedQueryQuery,
-    FeedQueryQueryVariables,
-    FeedQueryProps<TChildProps>
-  >
-) {
-  return ReactApollo.withQuery<
-    TProps,
-    FeedQueryQuery,
-    FeedQueryQueryVariables,
-    FeedQueryProps<TChildProps>
-  >(FeedQueryDocument, {
-    alias: "withFeedQuery",
-    ...operationOptions
-  });
-}
+  ({ variables: PostQueryQueryVariables; skip?: false } | { skip: true });
+
+export const PostQueryComponent = (props: PostQueryComponentProps) => (
+  <ReactApollo.Query<PostQueryQuery, PostQueryQueryVariables>
+    query={PostQueryDocument}
+    {...props}
+  />
+);
