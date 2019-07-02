d8bdcc8368e027ceb178b43f1f90520a97a29225
Stephen Jensen <stephen.jensen3@gmail.com>
2019-07-01 23:25:45 -0500

    cleaned up

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,33 +1,15 @@
 import * as React from 'react';
 import Layout from '../components/layout';
 import { NextPage } from 'next';
-
-import { FeedQueryComponent } from '../generated/apollo-components';
+import PostsList from '../components/posts';
+import NewDraft from '../components/new-draft';
 
 const IndexPage: NextPage = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Hello Next.js 👋</h1>
-      <FeedQueryComponent>
-        {({ loading, error, data }) => {
-          if (loading) return <p>Loading...</p>;
-          if (error) return <p>Error</p>;
-
-          if (data && 'feed' in data && data.feed.length > 0) {
-            return (
-              <React.Fragment>
-                {data.feed.map(({ title, content }, i) => (
-                  <p key={i}>
-                    {title}-{content}
-                  </p>
-                ))}
-              </React.Fragment>
-            );
-          }
-
-          return <p>No results yet.</p>;
-        }}
-      </FeedQueryComponent>
+      <NewDraft />
+      <PostsList />
     </Layout>
   );
 };
